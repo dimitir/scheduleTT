8616bba4222027a9086969d126624eb57ca4cb89
Dimitir <dimitir25@gmail.com>
2019-08-07 15:28:52 +0200

    вывод обьектов по дням

--- src/js/createEvent.js
+++ src/js/createEvent.js
@@ -1,22 +1,27 @@
 
-class AcceptingData {
+class EventRecording {
   constructor() {
-
   }
 
 
+  static get _getElement() {
+    // what do i do if element absent?
+    let objDomElem = {};
+    objDomElem.timeStart = document.getElementById('timeStart');
+    objDomElem.timeFinish = document.getElementById('timeFinish');
+    objDomElem.button = document.getElementById('buttonAddEvent');
+    objDomElem.warningStartNode = timeStart.parentNode.querySelector('.eventAdd_validaty');
+    objDomElem.warningFinishNode = timeFinish.parentNode.querySelector('.eventAdd_validaty');
+    return objDomElem;
+  }
+
   // get value from input timeStart and timeFinish 
-  static get getStartFinishTime() {
-    let timeVal = {}
-    let timeStart = document.getElementById('timeStart');
-    let timeFinish = document.getElementById('timeFinish');
+  static get _getStartFinishTime() {
+    let arrDom = EventRecording._getElement;
+    let timeVal = {};
 
-    timeVal.button = document.getElementById('buttonAddEvent');
-    timeVal.warningStart = timeStart.parentNode.querySelector('.eventAdd_validaty');
-    timeVal.warningFinish = timeFinish.parentNode.querySelector('.eventAdd_validaty');
-
-    let timeStartVal = timeStart.value;
-    let timeFinishVal = timeFinish.value;
+    let timeStartVal = arrDom.timeStart.value;
+    let timeFinishVal = arrDom.timeFinish.value;
 
     timeStartVal = timeStartVal.split(':');
     timeFinishVal = timeFinishVal.split(':');
@@ -32,48 +37,127 @@ class AcceptingData {
     return timeVal;
   }
 
-  // compare start and finish
+
+  static get _checkSimilarTime() {
+    // get data about check exist
+    let listElement = document.querySelectorAll('.eventList');
+    console.log(listElement);
+    let arrDate = {};
+    listElement.forEach(function (element) {
+      let day = element.querySelector('.eventList_date').innerHTML;
+      day = day.split('.');
+      day = day.reverse();
+      day = day.join(',');
+      day = new Date(day).getTime();
+
+      let eventRow = element.querySelectorAll('.eventRow');
+      let storEvent = {};
+      eventRow.forEach(function (line) {
+        let startEvent = line.querySelector('.eventRow_start').innerHTML;
+        startEvent = startEvent.split(':');
+        startEvent = startEvent[0] * 60 + +startEvent[1];
+        startEvent = startEvent + day;
+
+        let finishEvent = line.querySelector('.eventRow_finish').innerHTML;
+        finishEvent = finishEvent.split(':');
+        finishEvent = finishEvent[0] * 60 + +finishEvent[1];
+        finishEvent = finishEvent + day;
+        storEvent[startEvent] = finishEvent;
+        console.log(storEvent);
+      });
+      if (day in arrDate) {
+        let dayExist = arrDate[day];
+        let merger = Object.assign(dayExist, storEvent);
+        arrDate[day] = merger;
+        console.log(arrDate);
+      }
+      else {
+        arrDate[day] = storEvent;
+      }
+
+    });
+    // console.log(arrDate);
+  }
+
   compareStartFinishTime() {
+
     let timeFinish = document.getElementById('timeFinish');
     let timeStart = document.getElementById('timeStart');
-    let timeVal = AcceptingData.getStartFinishTime;
 
+    // when left timeFinish input
     timeFinish.addEventListener('blur', function () {
+      let timeVal = EventRecording._getStartFinishTime;
+      let arrDom = EventRecording._getElement;
 
       if ((timeVal.timeStartVal !== false && timeVal.timeFinishVal !== false)
         && (timeVal.timeStartVal >= timeVal.timeFinishVal)) {
-        timeVal.warningFinish.innerHTML = 'Мероприятие не может закончиться до начала!';
-        timeVal.warningFinish.style.visibility = "visible";
-        timeVal.button.dataset.timefinish = 'false';
+        arrDom.warningFinishNode.innerHTML = 'Мероприятие не может закончиться до начала!';
+        arrDom.warningFinishNode.style.visibility = "visible";
+        arrDom.button.dataset.timefinish = 'false';
       }
-      else {
-        timeVal.warningFinish.style.visibility = "hidden";
-        timeVal.button.dataset.timefinish = 'true';
+      else if ((timeVal.timeStartVal !== false && timeVal.timeFinishVal !== false)
+        && (timeVal.timeStartVal < timeVal.timeFinishVal)) {
+        arrDom.warningStartNode.style.visibility = "hidden";
+        arrDom.warningFinishNode.style.visibility = "hidden";
+        arrDom.button.dataset.timestart = 'true';
+        arrDom.button.dataset.timefinish = 'true';
       }
     });
 
     timeStart.addEventListener('blur', function () {
-      let timeVal = AcceptingData.getStartFinishTime;
+      let timeVal = EventRecording._getStartFinishTime;
+      let arrDom = EventRecording._getElement;
+
       if ((timeVal.timeStartVal !== false && timeVal.timeFinishVal !== false)
         && (timeVal.timeStartVal >= timeVal.timeFinishVal)) {
-        timeVal.warningStart.innerHTML = 'Машину времени вроде не придумали!';
-        timeVal.warningStart.style.visibility = "visible";
-        timeVal.button.dataset.timefinish = 'false';
+        arrDom.warningStartNode.innerHTML = 'Машину времени вроде не придумали!';
+        arrDom.warningStartNode.style.visibility = "visible";
+        arrDom.button.dataset.timestart = 'false';
       }
-      else {
-        timeVal.warningStart.style.visibility = "hidden";
-        timeVal.button.dataset.timefinish = 'true';
+      else if ((timeVal.timeStartVal !== false && timeVal.timeFinishVal !== false)
+        && (timeVal.timeStartVal < timeVal.timeFinishVal)) {
+        arrDom.warningStartNode.style.visibility = "hidden";
+        arrDom.warningFinishNode.style.visibility = "hidden";
+        arrDom.button.dataset.timefinish = 'true';
+        arrDom.button.dataset.timestart = 'true';
       }
-
-
     });
   }
 
-  getData() {
-    let obj = {};
-    document.getElementById('buttonAddEvent').addEventListener('click', function () {
-      var searchForm = document.forms["addEvent"];
-      console.log(searchForm);
+  formProcessing() {
+    let searchForm = document.forms["addEvent"];
+    let testVal = '2019,12,10';
+    var birthday = new Date(testVal);
+    // console.log(birthday);
+    console.log(birthday.getTime());
+
+    searchForm.addEventListener('submit', function (e) {
+      event.preventDefault();
+      let formVal = {};
+      let timeVal = EventRecording._getStartFinishTime;
+      let arrDom = EventRecording._getElement;
+
+      let dataTimeStart = arrDom.button.dataset.timestart;
+      let dataTimeFinish = arrDom.button.dataset.timefinish;
+      let formData = new FormData(searchForm);
+      formVal.fname = formData.get('fname');
+      formVal.dateEvent = formData.get('dateEvent').getTime();
+
+      formVal.timeStart = timeVal.timeStartVal;
+      formVal.timeFinish = timeVal.timeFinishVal;
+
+      if (dataTimeFinish !== false && dataTimeStart !== false) {
+        // console.log(formVal);
+
+        //  run check _similarTime
+        // run drow
+        // console.log(fname, dateEvent, timeFinish, timeStart);
+      }
+      else {
+        console.log(`что то пошло не так!`);
+      }
+
+      // formData.append('username', 'Chris');
       /*  obj.eventName = document.getElementById('eventName').value;
        obj.timeStart = document.getElementById('timeStart').value;
        obj.timeFinish = document.getElementById('timeFinish').value;
@@ -100,9 +184,10 @@ class AcceptingData {
 
 
 
-let acceptingData = new AcceptingData();
-acceptingData.compareStartFinishTime();
-acceptingData.getData();
+let eventRecording = new EventRecording();
+eventRecording.compareStartFinishTime();
+eventRecording.formProcessing();
+console.log(EventRecording._checkSimilarTime);
 
 
 
